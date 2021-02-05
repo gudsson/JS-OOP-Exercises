96d4dcb5dc96ef6168daa9d43b1239027427f97b
Jay Gudsson <gudsson@gmail.com>
2021-02-04 23:56:54 -0800

    lesson2 progress

---
+++ lesson_2/practice-problems/context-loss/pp3.js
@@ -0,0 +1,21 @@
+let turk = {
+  firstName: 'Christopher',
+  lastName: 'Turk',
+  occupation: 'Surgeon',
+  getDescription() {
+    return this.firstName + ' ' + this.lastName + ' is a '
+      + this.occupation + '.';
+  }
+};
+
+function logReturnVal(func) {
+  let returnVal = func();
+  console.log(returnVal);
+}
+
+let getTurkDescription = turk.getDescription.bind(turk);
+logReturnVal(getTurkDescription);
+
+// Suppose that we want to extract getDescription from turk,
+// but we always want it to execute with turk as its execution
+// context. How would you modify your code to do that?
