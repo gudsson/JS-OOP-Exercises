69e493ef451d3d7ee5bd1b1743b3f2d3ae5fe578
Jay Gudsson <gudsson@gmail.com>
2021-02-05 17:37:51 -0800

    finished lesson2

---
+++ lesson_2/practice-problems/context-loss/pp6.js
@@ -0,0 +1,12 @@
+const TESgames = {
+  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+  seriesTitle: 'The Elder Scrolls',
+
+  listGames: function() {
+    this.titles.forEach(function(title) {
+      console.log(this.seriesTitle + ': ' + title);
+    }, this);
+  }
+};
+
+TESgames.listGames();
