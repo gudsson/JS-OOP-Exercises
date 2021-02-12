f67ee1dc291875c085615f7a7696f9ad87d272a2
Jay Gudsson <gudsson@gmail.com>
2021-02-12 13:01:39 -0800

    init ttt

---
+++ lesson_5/ttt.js
@@ -0,0 +1,17 @@
+// textual description of the game.
+
+// - Tic Tac Toe is a 2-player board game.
+// - The board is a 3x3 grid.
+// - Players take turns marking a square with a marker
+//   that identifies the player.
+// - Traditionally, the player to go first uses the marker
+//   X to mark her squares, and the player to go second uses
+//   the marker O.
+// - The first player to mark 3 squares in a row with
+//   her marker wins the game.
+// - A row can be a horizontal row, a vertical column,
+//   or either of the two diagonals (top-left to bottom-right
+//   and top-right to bottom-left).
+// - There is one human player and one computer player.
+// - The human player always moves (places a marker) first
+//   in the initial version of our game; you can change that later.
