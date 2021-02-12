2e8112a5ea4160f77362e2cfb7d052522ac1f23b
Jay Gudsson <gudsson@gmail.com>
2021-02-11 21:12:10 -0800

    js120 exercises complete

--- exercises/object-creation-patterns/classical-object-creation.js
+++ exercises/object-creation-patterns/classical-object-creation.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 ///////////////////////////////
 // Classical Object Creation //
 ///////////////////////////////
@@ -14,9 +15,61 @@ function Person(firstName, lastName, age, gender) {
   this.lastName = lastName;
   this.age = age;
   this.gender = gender;
-
 }
 
+Person.prototype.eat = function() {
+  console.log('Eating');
+};
+
+Person.prototype.communicate = function() {
+  console.log('Communicating');
+};
+
+Person.prototype.sleep = function() {
+  console.log('Sleeping');
+};
+
+Person.prototype.fullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+function Doctor(firstName, lastName, age, gender, specialty) {
+  Person.call(this, firstName, lastName, age, gender);
+  this.specialty = specialty;
+}
+Doctor.prototype = Object.create(Person.prototype);
+
+Doctor.prototype.diagnose = function() {
+  console.log('Diagnosing');
+};
+
+function Professor(firstName, lastName, age, gender, subject) {
+  Person.call(this, firstName, lastName, age, gender);
+  this.subject = subject;
+}
+Professor.prototype = Object.create(Person.prototype);
+
+Professor.prototype.teach = function() {
+  console.log('teaching');
+};
+
+function Student(firstName, lastName, age, gender, degree) {
+  Person.call(this, firstName, lastName, age, gender);
+  this.degree = degree;
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.study = function() {
+  console.log('Studying');
+};
+
+function GraduateStudent(firstName, lastName, age, gender, degree, graduateDegree) {
+  Student.call(this, firstName, lastName, age, gender, degree);
+  this.graduateDegree = graduateDegree;
+}
+GraduateStudent.prototype = Object.create(Student.prototype);
+GraduateStudent.prototype.research = function() {
+  console.log('Researching');
+};
 
 // Here's a sample run you can use as a reference:
 
@@ -28,24 +81,24 @@ person.communicate();                      // logs 'Communicating'
 person.sleep();                            // logs 'Sleeping'
 console.log(person.fullName());            // logs 'foo bar'
 
-// let doctor = new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics');
+let doctor = new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics');
 
-// console.log(doctor instanceof Person);     // logs true
-// console.log(doctor instanceof Doctor);     // logs true
-// doctor.eat();                              // logs 'Eating'
-// doctor.communicate();                      // logs 'Communicating'
-// doctor.sleep();                            // logs 'Sleeping'
-// console.log(doctor.fullName());            // logs 'foo bar'
-// doctor.diagnose();                         // logs 'Diagnosing'
+console.log(doctor instanceof Person);     // logs true
+console.log(doctor instanceof Doctor);     // logs true
+doctor.eat();                              // logs 'Eating'
+doctor.communicate();                      // logs 'Communicating'
+doctor.sleep();                            // logs 'Sleeping'
+console.log(doctor.fullName());            // logs 'foo bar'
+doctor.diagnose();                         // logs 'Diagnosing'
 
-// let graduateStudent = new GraduateStudent('foo', 'bar', 21, 'gender', 'BS Industrial Engineering', 'MS Industrial Engineering');
+let graduateStudent = new GraduateStudent('foo', 'bar', 21, 'gender', 'BS Industrial Engineering', 'MS Industrial Engineering');
 // // logs true for next three statements
-// console.log(graduateStudent instanceof Person);
-// console.log(graduateStudent instanceof Student);
-// console.log(graduateStudent instanceof GraduateStudent);
-// graduateStudent.eat();                     // logs 'Eating'
-// graduateStudent.communicate();             // logs 'Communicating'
-// graduateStudent.sleep();                   // logs 'Sleeping'
-// console.log(graduateStudent.fullName());   // logs 'foo bar'
-// graduateStudent.study();                   // logs 'Studying'
-// graduateStudent.research();                // logs 'Researching'
+console.log(graduateStudent instanceof Person);
+console.log(graduateStudent instanceof Student);
+console.log(graduateStudent instanceof GraduateStudent);
+graduateStudent.eat();                     // logs 'Eating'
+graduateStudent.communicate();             // logs 'Communicating'
+graduateStudent.sleep();                   // logs 'Sleeping'
+console.log(graduateStudent.fullName());   // logs 'foo bar'
+graduateStudent.study();                   // logs 'Studying'
+graduateStudent.research();                // logs 'Researching'
