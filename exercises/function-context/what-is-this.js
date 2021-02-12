24d9d726614e44918d4ea5b753ec3f5d672a0ef3
Jay Gudsson <gudsson@gmail.com>
2021-02-11 17:52:09 -0800

    func context exercises complete

---
+++ exercises/function-context/what-is-this.js
@@ -0,0 +1,17 @@
+///////////////////
+// What is This? //
+///////////////////
+
+// Read the following code carefully. What do you think is logged on line 7.
+// Try to answer the question before you run the code.
+
+let person = {
+  firstName: 'Rick ',
+  lastName: 'Sanchez',
+  // fullName: this.firstName + this.lastName,
+  fullName() {
+    return this.firstName + this.lastName;
+  }
+};
+
+console.log(person.fullName());
