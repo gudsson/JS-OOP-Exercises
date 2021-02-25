ef934bb535af0691c200ddfc636d3c2ead9b10ef
Jay Gudsson <gudsson@gmail.com>
2021-02-24 22:58:45 -0800

    21 deal hands

--- lesson_5/oo_twentyone.js
+++ lesson_5/oo_twentyone.js
@@ -20,27 +20,97 @@
 //      bust (state)
 //      Score (n, state)
 
+const readline = require('readline-sync');
+
 class Card {
-  constructor(value, suit) {
-    this.value = value;
+  constructor(rank, suit) {
+    this.rank = rank;
     this.suit = suit;
+    this.hide = false;
   }
 
-  // getValue
+  static SUITS = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
+
+  static RANKS = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    Jack: 10,
+    Queen: 10,
+    King: 10,
+    Ace: 11
+  }
+
+  getCardName() {
+    return `${this.rank} of ${this.suit}`;
+  }
+
+  getRank() {
+    return this.rank;
+  }
+
+  isAce() {
+    return this.rank === 'Ace';
+  }
+
+  getPointValue() {
+    return null;
+  }
+
+  hideCard() {
+    this.hide = true;
+  }
+
+  unhideCard() {
+    this.hide = false;
+  }
 }
 
 class Deck {
   constructor() {
-    //STUB
+    this.cardsInDeck = [];
+    this.buildDeck();
   }
 
-  deal() {
-    //STUB
+  buildDeck() {
+    Object.keys(Card.RANKS).forEach(rank => {
+      Card.SUITS.forEach(suit => {
+        let newCard = new Card(rank, suit);
+        this.cardsInDeck.push(newCard);
+      });
+    });
+  }
+
+  countRemainingCards() {
+    return this.cardsInDeck.length;
+  }
+
+  dealCard() {
+    let randomCardIdx = Math.floor(this.countRemainingCards() * Math.random());
+    return this.cardsInDeck.splice(randomCardIdx, 1)[0];
   }
 }
 
 class Participant {
   constructor() {
+    this.hand = [];
+    this.handValue = 0;
+  }
+
+  // dealHand() {
+  //   this.hand
+  // }
+  takeCard(card) {
+    this.hand.push(card);
+  }
+
+  displayHand() {
     //STUB
   }
 
@@ -63,40 +133,99 @@ class Participant {
 
 class Player extends Participant {
   constructor() {
-    //STUB
     super();
+    this.wallet = Player.STARTING_CASH;
+  }
+
+  static STARTING_CASH = 5;
+  static WINNING_THRESHOLD = 2 * Player.STARTING_CASH;
+  static BET_AMOUNT = 1;
+
+  getCashOnHand() {
+    return this.wallet;
+  }
+
+  showCashOnHand() {
+    console.log(`$${this.getCashOnHand}`);
+  }
+
+  placeBet() {
+    this.wallet -= Player.BET_AMOUNT;
+  }
+
+  addWinnings() {
+    this.wallet += Player.BET_AMOUNT * 2;
+  }
+
+  isBroke() {
+    return this.wallet <= 0;
+  }
+
+  isRich() {
+    return this.wallet >= Player.WINNING_THRESHOLD;
   }
 }
 
 class Dealer extends Participant {
   constructor() {
-    //STUB
     super();
   }
+
+  static HIT_THRESHOLD = 17;
+
+  showHoleCard() {
+
+  }
+
+  hideHoleCard() {}
 }
 
 class TwentyOneGame {
   constructor() {
     //STUB
+    this.deck = new Deck();
+    this.player = new Player();
+    this.dealer = new Dealer();
   }
 
   start() {
     //SPIKE
     this.displayWelcomeMessage();
-    this.dealCards();
-    this.showCards();
-    this.playerTurn();
-    this.dealerTurn();
+
+    //playSingleHand
+    while (true) {
+      this.playSingleHand();
+      // if (this.player.isBroke() || this.player.isRich()) break;
+      if (!this.playAgain()) break;
+    }
+
+
     this.displayResult();
     this.displayGoodbyeMessage();
   }
 
+  playSingleHand() {
+    this.dealCards();
+    this.showCards();
+    // this.playerTurn();
+    // this.dealerTurn();
+    let pause = readline.question();
+  }
+
   dealCards() {
-    //STUB
+    for (let idx = 0; idx < 2; idx++) {
+      this.player.takeCard(this.deck.dealCard());
+      this.dealer.takeCard(this.deck.dealCard());
+    }
   }
 
   showCards() {
     //STUB
+    console.log(this.player.hand);
+    console.log(this.dealer.hand);
+    // this.deck.cardsInDeck.forEach(card => {
+    //   console.log(card.getCardName());
+    // });
   }
 
   playerTurn() {
@@ -108,15 +237,32 @@ class TwentyOneGame {
   }
 
   displayWelcomeMessage() {
-    //STUB
+    console.clear();
+    console.log("Welcome to Twenty-One!");
+    console.log("");
   }
 
   displayGoodbyeMessage() {
-    //STUB
+    console.log("\nThanks for playing Tic Tac Toe!  Goodbye!");
   }
 
   displayResult() {
     //STUB
+  }
+
+  playAgain() {
+    let answer;
+
+    while (true) {
+      console.log("Would you like to play again? ('y' or 'n')");
+      answer = readline.question("=> ").toLowerCase();
+
+      if (['y', 'n'].includes(answer)) break;
+
+      console.log('\nSorry, invalid choice');
+    }
+
+    return answer === 'y';
   }
 }
 
